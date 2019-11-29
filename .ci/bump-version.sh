89a711bad86d15f600958565a1f7fc2fefc94fee
Victor Martinez <victormartinezrubio@gmail.com>
2019-11-29 14:03:42 +0000

    ci: release process with more test coverage (#27)

---
+++ .ci/bump-version.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+AGENT_VERSION="${1?Missing the APM rum agent version}"
+
+# Bump version
+sed -ibck "s#--branch='master'#--branch='${AGENT_VERSION}'#g" package.json
+
+# Commit changes
+git add package.json
+git commit -m "Bump version ${AGENT_VERSION}"
